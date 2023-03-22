77e344822b5360cb33f103eb932ad80d46b8881d
pannawatfriend <friendlysripa@gmail.com>
2023-03-22 11:38:44 +0700

    3 commands

--- src/myproject1/Myproject1.java
+++ src/myproject1/Myproject1.java
@@ -15,6 +15,8 @@ public class Myproject1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("second");
+        System.out.println("hello Programing");
     }
     
 }
